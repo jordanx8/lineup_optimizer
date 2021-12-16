756b2af7625d64e74d3d82e879728081bf91857a
Jordan Gary <60443094+jordanx8@users.noreply.github.com>
2021-12-16 10:00:06 -0600

    removed lineup struct

--- player/player.go
+++ player/player.go
@@ -7,19 +7,6 @@ type player struct {
 	points    int
 }
 
-type lineup struct {
-	PG    player
-	SG    player
-	G     player
-	SF    player
-	PF    player
-	F     player
-	C     player
-	C2    player
-	Util  player
-	Util2 player
-}
-
 func newPlayer(name string, positions []string, team string, points int) *player {
 	p := player{name: name, positions: positions, team: team, points: points}
 	return &p
