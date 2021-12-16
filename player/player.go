d96df19a019506601d04cf574a01ebf6a37abd20
Jordan Gary <60443094+jordanx8@users.noreply.github.com>
2021-12-16 13:55:32 -0600

    setLineup updated
    - checks for one depth if player can be moved to add another player
    - doesn't remove said player from availableplayers array yet

--- player/player.go
+++ player/player.go
@@ -30,6 +30,30 @@ func setLineup(availableplayers []player, lineup map[string]player) (map[string]
 				fmt.Println(value, "already at position", availableplayers[j].positions[i])
 				if i == len(availableplayers[j].positions)-1 {
 					fmt.Println("No open spots available for", availableplayers[j])
+					k := 0
+					fmt.Println("Checking if another player can be moved to make room for", availableplayers[j])
+					for k < len(availableplayers[j].positions) {
+						if value, ok := lineup[availableplayers[j].positions[k]]; ok {
+							fmt.Println("Checking if", value, "at", availableplayers[j].positions[k], "can be moved")
+							l := 0
+							for l < len(value.positions) {
+								if value2, ok := lineup[value.positions[l]]; ok {
+									fmt.Println(value2, "already at position", value.positions[l])
+
+								} else {
+									fmt.Println(value.positions[l], "IS OPEN")
+									lineup[value.positions[l]] = value
+									fmt.Println(value, "has been moved from", availableplayers[j].positions[k], "to", value.positions[l])
+									lineup[availableplayers[j].positions[k]] = availableplayers[j]
+									fmt.Println("Adding", availableplayers[j], "to position", availableplayers[j].positions[k])
+								}
+								l++
+							}
+						}
+
+						k++
+
+					}
 				}
 			} else {
 				fmt.Println(availableplayers[j].positions[i], "position is open, adding", availableplayers[j], "to lineup")
