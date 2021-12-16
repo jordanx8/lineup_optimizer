7644f9230c5448554a801ba324a894e1d8774373
Jordan Gary <60443094+jordanx8@users.noreply.github.com>
2021-12-16 10:16:05 -0600

    lineup map

--- player/player.go
+++ player/player.go
@@ -7,6 +7,11 @@ type player struct {
 	points    int
 }
 
+//	lineup := make(map[string]player)
+//	lineup["PG"] = *p
+//	lineup["SG"] = *p2
+//	fmt.Println(lineup)
+
 func newPlayer(name string, positions []string, team string, points int) *player {
 	p := player{name: name, positions: positions, team: team, points: points}
 	return &p
