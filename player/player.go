001332d318ed8ca7e4c0824b90ccf0d65d59357a
Jordan Gary <60443094+jordanx8@users.noreply.github.com>
2021-12-16 10:43:24 -0600

    orderPlayers function

--- player/player.go
+++ player/player.go
@@ -16,3 +16,10 @@ func newPlayer(name string, positions []string, team string, points int) *player
 	p := player{name: name, positions: positions, team: team, points: points}
 	return &p
 }
+
+func orderPlayers(availableplayers []player) []player {
+	sort.SliceStable(availableplayers, func(i, j int) bool {
+		return availableplayers[i].points > availableplayers[j].points
+	})
+	return availableplayers
+}
