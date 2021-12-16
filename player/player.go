6b027b5a3096b6fd230860798711f255948dcacc
Jordan Gary <60443094+jordanx8@users.noreply.github.com>
2021-12-16 11:58:24 -0600

    func setLineup

--- player/player.go
+++ player/player.go
@@ -17,6 +17,28 @@ func newPlayer(name string, positions []string, team string, points int) *player
 	return &p
 }
 
+func setLineup(availableplayers []player, lineup map[string]player) map[string]player {
+	i := 0
+	j := 0
+
+	for j < len(availableplayers) {
+		i = 0
+		for i < len(availableplayers[j].positions) {
+			if value, ok := lineup[availableplayers[j].positions[i]]; ok {
+				fmt.Println("Player already at this position: ", availableplayers[j].positions[i])
+				fmt.Println("value: ", value)
+			} else {
+				fmt.Println(availableplayers[j].positions[i], "position is open, adding player to lineup")
+				lineup[availableplayers[j].positions[i]] = availableplayers[j]
+				i = len(availableplayers[j].positions) //end loop when player is added to lineup
+			}
+			i++
+		}
+		j++
+	}
+	return lineup
+}
+
 func orderPlayers(availableplayers []player) []player {
 	sort.SliceStable(availableplayers, func(i, j int) bool {
 		return availableplayers[i].points > availableplayers[j].points
