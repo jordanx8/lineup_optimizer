dfa1cb5008e2d36a5065052b076de72e3bdf4178
jordanx8 <froggyairplane@gmail.com>
2021-12-18 21:39:02 -0600

    chromedp day 1
    - got login working, gets to fantasy basketball page

--- go.mod
+++ go.mod
@@ -3,9 +3,25 @@ module github.com/jordanx8/lineup_optimizer
 go 1.16
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.9 // indirect
+	github.com/chromedp/cdproto v0.0.0-20211205231339-d2673e93eee4 // indirect
+	github.com/chromedp/chromedp v0.7.6 // indirect
 	github.com/go-vgo/robotgo v0.100.10
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
 
-	"github.com/go-vgo/robotgo"
+	cdp "github.com/chromedp/chromedp"
 )
 
 func main() {
-	robotgo.Move(345, 51)
-	robotgo.Click()
-	robotgo.TypeStr("https://basketball.fantasysports.yahoo.com/")
-	robotgo.KeyTap("enter")
-	robotgo.Sleep(5)
-	robotgo.Move(765, 522)       //my team
-	robotgo.MoveSmooth(775, 586) //edit weekly lineup
-	robotgo.Click()              //left click
-	x, y := robotgo.GetMousePos()
-	fmt.Println("pos: ", x, y)
+	var username string
+	var password string
+
+	fmt.Println("Enter email address/username: ")
+	fmt.Scanln(&username)
+
+	fmt.Println("Enter password: ")
+	fmt.Scanln(&password)
+
+	// create chrome instance
+	ctx, cancel := cdp.NewContext(
+		context.Background(),
+		// chromedp.WithDebugf(log.Printf),
+	)
+	defer cancel()
+
+	// create a timeout
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	var buf []byte
+	err := cdp.Run(ctx,
+		cdp.Navigate(`https://login.yahoo.com/`),
+		cdp.WaitVisible(`username-challenge`),
+		cdp.SetValue(`phone-no`, username),
+		cdp.Click(`login-signin`, cdp.NodeVisible),
+		cdp.WaitVisible(`password-container`),
+		cdp.SetValue(`login-passwd`, password),
+		cdp.Click(`login-signin`, cdp.NodeVisible),
+		cdp.WaitVisible(`ybar-logo`),
+		cdp.Navigate(`https://basketball.fantasysports.yahoo.com/`),
+		cdp.Sleep(2*time.Second),
+		cdp.CaptureScreenshot(&buf),
+	)
+
+	ioutil.WriteFile("screenshot.png", buf, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
